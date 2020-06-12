33e3c01b10907fbbd8ad0790e449d2bb80518e10
jc durbin <jcdurbin@hotmail.com>
2020-06-12 09:27:22 +0100

    es6/7 version

--- test/index.js
+++ test/index.js
@@ -1,53 +1,48 @@
-var syncFetch = new syncFetch();
-var container = document.getElementById('results');
+var syncFetch = SyncFetch();
+var container = document.getElementById("results");
 
-function element(data){    
-    var element = document.createElement('div');
-    element.innerHTML = data;
+function element(data) {
+  var element = document.createElement("div");
+  element.innerHTML = data;
 
-    container.appendChild(element);
-
+  container.appendChild(element);
 }
 
 function success(data) {
-    console.log("Successful result", data);
-    element(JSON.stringify(data));
+  console.log("Successful result", data);
+  element(JSON.stringify(data));
 }
 
 function fail(data) {
-    console.error("Failed result", data);
-    element(data);
+  console.error("Failed result", data);
+  element(data);
 }
 
-function debugData(data){
-    console.log("Debug Info", data);
-    element(data);
-}
-   
-
-    
-function addQueueItems(){
-    let url = document.querySelector('[name="url"]').value;
-    let qty = document.querySelector('[name="qty"]').value;
-
-    var queueItemObj = {
-        url: url,
-        success: success,
-        fail: fail,
-        responseType: 'json',
-        header: true,
-        headers: ['content-type'],        
-        debug: true,        
-        debugData: debugData
-    };
-
-    while(container.lastChild){
-        container.lastChild.remove();
-    }
-
-    for (var i = 0; i < qty; i++) {
-        syncFetch.add(queueItemObj);    
-     
-    }
+function debugData(data) {
+  console.log("Debug Info", data);
+  element(data);
 }
 
+function addQueueItems() {
+  let url = document.querySelector('[name="url"]').value;
+  let qty = document.querySelector('[name="qty"]').value;
+
+  var queueItemObj = {
+    url: url,
+    success: success,
+    fail: fail,
+    responseType: "json",
+    header: true,
+    headers: ["content-type"],
+    debug: true,
+    debugData: debugData,
+  };
+
+  while (container.lastChild) {
+    container.lastChild.remove();
+  }
+
+  for (var i = 0; i < qty; i++) {
+    syncFetch.add(queueItemObj);
+  }
+}
